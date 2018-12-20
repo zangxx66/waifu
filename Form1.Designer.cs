79cace626e860c7730a9d3f833a72c58670b088e
zangxx66 <thespirit@vip.qq.com>
2018-12-20 22:08:40 +0800

    commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,31 +31,40 @@
             this.label1 = new System.Windows.Forms.Label();
             this.buttonExit = new System.Windows.Forms.Button();
             this.hair_text = new System.Windows.Forms.Label();
+            this.Attr_label = new System.Windows.Forms.Label();
+            this.Attr_text = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // 发色label
             // 
             this.label1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "hair_label";
-            this.label1.Size = new System.Drawing.Size(400, 376);
+            this.label1.Size = new System.Drawing.Size(200, 200);
             this.label1.TabIndex = 1;
             this.label1.Text = "发色：";
-            //this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-
             //
             // 发色text
             //
             this.hair_text.Dock = System.Windows.Forms.DockStyle.Right;
-            this.hair_text.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hair_text.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.hair_text.Location = new System.Drawing.Point(400,0);
             this.hair_text.Name = "hair_text";
-            this.hair_text.Size = new System.Drawing.Size(400,376);
+            this.hair_text.Size = new System.Drawing.Size(400,200);
             this.hair_text.TabIndex = 2;
-            this.hair_text.Text = "";
-            // this.hair_text.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-
+            this.hair_text.Text = "";           
+            //
+            //属性label
+            //
+            this.Attr_label.Dock = System.Windows.Forms.DockStyle.Left;
+            this.Attr_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Attr_label.Location = new System.Drawing.Point(0,800);
+            this.Attr_label.Name = "Attr_label";
+            this.Attr_label.Size = new System.Drawing.Size(200,200);
+            this.Attr_label.AutoSize = true;
+            this.Attr_label.TabIndex = 3;
+            this.Attr_label.Text = "属性：";
             // 
             // buttonExit
             // 
@@ -74,10 +83,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 800);
+            this.ClientSize = new System.Drawing.Size(1000, 1200);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonExit);
             this.Controls.Add(this.hair_text);
+            this.Controls.Add(this.Attr_label);
             this.Name = "waifu";
             this.Text = "老婆生成器v0.0.1";
             this.ResumeLayout(false);
@@ -89,6 +99,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button buttonExit;
         private System.Windows.Forms.Label hair_text;
+        private System.Windows.Forms.Label Attr_label;
+        private System.Windows.Forms.Label Attr_text;
     }
 }
 
